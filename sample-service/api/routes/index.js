7933d1095280be83443237c4ecb9e1387f9a2948
Evgeniy <ekovalwork@gmail.com>
2019-12-09 10:26:04 +0200

    Search by room name

--- sample-service/api/routes/index.js
+++ sample-service/api/routes/index.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const UserController = require('../controllers/UserController');
 const MainController = require('../controllers/MainController');
 const ChatController = require('../controllers/ChatController');
+const RoomController = require('../controllers/RoomController');
 
 router.get('/', MainController.index);
 
@@ -16,6 +17,9 @@ router.post('/register', UserController.store);
 router.get('/profile/:id', UserController.edit);
 router.put('/profile/:id', UserController.update);
 router.post('/logout', UserController.logout);
+
+router.get('/api/room/:id', RoomController.show);
+router.put('/api/room', RoomController.update);
 
 router.get('/api/chat/:id', ChatController.show);
 router.post('/api/chat', ChatController.store);
